df20f7c251e45e192621e801c06bbd01abcf3939
Raman Patsiayuk <raman_patsiayuk@epam.com>
2017-02-15 18:06:12 +0300

    updated CucumberTest

--- RestAssured/src/test/java/com/openweather/by_city_id/CucumberTest.java
+++ RestAssured/src/test/java/com/openweather/by_city_id/CucumberTest.java
@@ -1,15 +1,11 @@
 package com.openweather.by_city_id;
 
 import com.openweather.BaseTest;
-import com.openweather.builder.OpenWeatherBuilderRequest;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.get;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static io.restassured.RestAssured.expect;
+import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 
 /**
@@ -19,19 +15,27 @@ public class CucumberTest extends BaseTest {
 
     @Test
     public void verifyCityGeoLocationLongitudeTest() {
-                    /* expect()
+                     expect()
                     .statusCode(200)
-                    .body("id", equalTo("629634"))
+                    .body("id", equalTo(629634))
                     .when()
-                    .get("http://api.openweathermap.org/data/2.5/weather?q=Brest,by&APPID=c893442b753870781d1f92e6457cd9a3");*/
+                    .get("http://api.openweathermap.org/data/2.5/weather?q=Brest,by&APPID=c893442b753870781d1f92e6457cd9a3");
 
-       /* given().contentType("application/json").
-                when().
-                get("http://api.openweathermap.org/data/2.5/weather?q=Brest,by&APPID=c893442b753870781d1f92e6457cd9a3").
-                then().
-                statusCode(200)
-                .and()
-                .assertThat()
-                .body("coord.lon", equalTo(23.7), "coord.lat", equalTo(52.0));*/
+           /* given().contentType("application/json").
+                    when().
+                    get("http://api.openweathermap.org/data/2.5/weather?q=Brest,by&APPID=c893442b753870781d1f92e6457cd9a3").
+                    then()
+                    .statusCode(200)
+                    .and()
+                    .assertThat()
+                    .body("coord.lon", equalTo(23.7F), "coord.lat", equalTo(52.1F));*/
+
+
+
+       /* Response res = when().get("http://api.openweathermap.org/data/2.5/weather?q=Brest,by&APPID=c893442b753870781d1f92e6457cd9a3");
+        JsonPath jp = new JsonPath(res.asString());
+        Float account = jp.getFloat("coord.lon");*/
     }
+
+
 }

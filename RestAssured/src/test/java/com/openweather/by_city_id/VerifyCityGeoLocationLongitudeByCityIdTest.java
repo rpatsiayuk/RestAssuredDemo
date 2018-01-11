4873cbd228899f9ac5c042739fd2af47b38097b1
Raman Patsiayuk <raman_patsiayuk@epam.com>
2018-01-11 16:32:26 +0300

    added log4j2.properties

--- RestAssured/src/test/java/com/openweather/by_city_id/VerifyCityGeoLocationLongitudeByCityIdTest.java
+++ RestAssured/src/test/java/com/openweather/by_city_id/VerifyCityGeoLocationLongitudeByCityIdTest.java
@@ -3,11 +3,17 @@ package com.openweather.by_city_id;
 import com.openweather.BaseTest;
 import com.openweather.builder.OpenWeatherBuilderRequest;
 import io.qameta.allure.Feature;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.JsonConfig.jsonConfig;
+import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -30,5 +36,20 @@ public class VerifyCityGeoLocationLongitudeByCityIdTest extends BaseTest {
         JsonPath jsonPath = new JsonPath(json);
         float actualResult = jsonPath.get("coord.lon");//получили значение элемента coord.lon
         assertThat(actualResult, equalTo(expectedResult)); // сравнили значение возвращаемое с сервиса, со значением, которое мы ожидаем
+    }
+
+    @Test
+    public void verifyCityGeoLocationLongitudeCucumberTest() {
+        String request = new OpenWeatherBuilderRequest().setCityId("629634").build();
+        given()
+                //.config(RestAssuredConfig.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL)))
+                .log()
+                .all()
+                .when()
+                .get(request)
+                .then()
+                .assertThat()
+                //.body("coord.lon", is(new BigDecimal(23.7)));
+                .body("coord.lon", is(23.7F));
     }
 }
